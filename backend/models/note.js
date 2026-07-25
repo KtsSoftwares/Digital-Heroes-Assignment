c465ae984c8bf85e6c24879c1f2d72eda4d6da5c
Akib Ali <info.ktsgroupglp@gmail.com>
2026-07-25 15:23:32 +0530

    Delete backend/models directory

--- backend/models/note.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const noteSchema = new mongoose.Schema({
-  leadId: { type: mongoose.Schema.Types.ObjectId, ref: 'Lead', required: true },
-  authorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true }
-}, { timestamps: true, collection: 'Notes' });
-
-module.exports = mongoose.model('Note', noteSchema);
